a6c3a84011934fc45a3d5223d8e774305ab8b401
Antonio Espinoza <anespinoza@baufest.com>
2024-06-01 19:56:36 -0400

    add bcrypt package

--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -1,6 +1,18 @@
+import { BcryptAdapter } from "../../config";
+import { UserModel } from "../../data/mongodb";
 import { AuthDataSource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { UserMapper } from "../mappers/user.mapper";
+
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
+
 
 export class AuthDataSourceImpl implements AuthDataSource {
+
+  constructor(
+    private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+    private readonly compareFunction: CompareFunction = BcryptAdapter.compare,
+  ) {}
 
 
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
@@ -11,10 +23,20 @@ export class AuthDataSourceImpl implements AuthDataSource {
 
       // 1. Verificar si el correo existe
 
+      const exists = await UserModel.findOne({email});
+      if(exists) throw CustomError.badRequest('User already exists');
+      
       // 2. Hash de contraseña
+      const user = await UserModel.create({
+        name: name,
+        email: email,
+        password: this.hashPassword(password),
+      });
+
+      await user.save();
 
       // 3. Mapear la respuesta a nuestra entidad
-      return new UserEntity('1',name, email, password, ['ADMIN_ROLE'],);
+      return UserMapper.userEntityFromObject(user);
 
     } catch (error) {
       if (error instanceof CustomError) {
